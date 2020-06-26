6b0dabb8da9fb3cbc445564d984d0b0013a1f69b
Ross Keenan <rkeenan@auroracoop.com>
2020-06-26 11:51:11 -0500

    Added Columns names for both sheets as variables

--- CC Reconciler.py
+++ CC Reconciler.py
@@ -9,9 +9,6 @@ READFILE1 = (r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\Developm
 wb1 = load_workbook(READFILE1, data_only=True)
 SHEET1 = wb1['Sheet1']
 
-
-
-
 for row in range(3, SHEET1.max_row + 1):
     with open(r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\Development\Account Reconciler\WriteFile1.txt', 'a') as f1:
         POSTDATE = SHEET1['A' + str(row)].value
@@ -34,6 +31,7 @@ for row in range(3, SHEET1.max_row + 1):
 print("Your write file #1 is done!")
 wb1.close()
 
+
 READFILE2 = (r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\Development\Account Reconciler\Copy of May Credit Card Reconciliation to GL.xlsx')
 wb2 = load_workbook(READFILE2, data_only=True)
 SHEET2 = wb2['GeneralLedgerDetailReportList']
@@ -41,10 +39,28 @@ DATE_RANGE = pd.to_datetime('2020-01-01').date()
 
 for row in range(7, SHEET1.max_row + 1):
     with open(r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\Development\Account Reconciler\WriteFile2.txt', 'a') as f1:
+        GL = SHEET2['A' + str(row)].value
+        PC = SHEET2['B' + str(row)].value
+        SOURCE_NAME = SHEET2['C' + str(row)].value
+        ACCOUNT = SHEET2['D' + str(row)].value
+        PERSON = SHEET2['E' + str(row)].value
+        NAME = SHEET2['I' + str(row)].value       
         DATE = SHEET2['I' + str(row)].value
         DATE = pd.to_datetime(DATE).date()
+        TICKET = SHEET2['J' + str(row)].value
+        TYPE = SHEET2['K' + str(row)].value
+        DEBIT = SHEET2['L' + str(row)].value
+        CREDIT = SHEET2['M' + str(row)].value
+        QUANTITY = SHEET2['N' + str(row)].value
+        RUNNING_BALANCE = SHEET2['O' + str(row)].value
+        SOURCE_DESCRIPTION = SHEET2['P' + str(row)].value
+        ACE_LOCATION = SHEET2['Q' + str(row)].value
+        IMPORT_CLEARED = SHEET2['R' + str(row)].value
+        COMMENTS = SHEET2['S' + str(row)].value
+
         if DATE < DATE_RANGE:
             print(str(DATE) + " is in range")
+            f1.write(str(GL) + "\t" + str(PC) + "\t" + str(SOURCE_NAME) + "\t" + str(ACCOUNT) + "\t" + str(PERSON) + "\t" + str(NAME) + "\t" + str(DATE) + "\t" + str(TICKET) + "\t" + str(TYPE) + "\t" + str(DEBIT) + "\t" + str(CREDIT) + "\t" + str(QUANTITY) + "\t" + str(RUNNING_BALANCE) + "\t" + str(SOURCE_DESCRIPTION) + "\t" + str(ACE_LOCATION) + "\t" + str(IMPORT_CLEARED) + "\t" + str(COMMENTS) + '\n')
         else:
             break
 
